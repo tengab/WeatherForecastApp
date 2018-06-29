8b37a34b3042c2d507cdf69e2f599a160a7028b3
Tomasz Banach <tomasz.banach@britenet.com.pl>
2018-06-29 16:00:52 +0200

    custom directive made

--- app.js
+++ app.js
@@ -62,3 +62,18 @@ weatherApp.controller('forecastController', ['$scope', '$resource', '$routeParam
     }
 
 }])
+
+// DIRECTIVES
+weatherApp.directive('weatherReport', function () {
+    return {
+        restrict: 'E',
+        templateUrl: 'directives/weatherReport.html',
+        replace: true,
+        scope: {
+            weatherDay: '=',
+            convertToStandard: '&',
+            convertToDate: '&',
+            dateFormat: '@'
+        }
+    }
+})
